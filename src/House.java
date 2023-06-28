d4ddc11efd56d255c311e4d2765eb05cc85f8ec7
Cesar Cotrina <101176031+Cezar1405@users.noreply.github.com>
2023-06-28 20:29:56 +0200

    Add files via upload

--- src/House.java
+++ src/House.java
@@ -1,17 +1,12 @@
 public class House {
-    //deiniendo variables
-    String adress;
-    int numberOfFloors;
+    //definiendo instancias
+    private String adress;
+    private int numberOfFloors;
 
     //Metodo Constuctor
     public House(String adress, int numberOfFloors) {
         this.adress = adress;
         this.numberOfFloors = numberOfFloors;
-    }
-
-    //Metodo para mostrar datos en pantalla
-    public void mostrarDatos(){
-        System.out.println("Direccion: "+adress);
-        System.out.println("Pisos: "+numberOfFloors);
+        System.out.println("Objeto creado correctamente.");
     }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,8 +1,20 @@
 public class Tester {
     public static void main(String[] args){
+        /*
+        Cree una clase llamada Casa que tenga 3 variables de instancia:
+        una String address.
+        un int numberOfFloors.
+        Defina un método constructor para House que imprimirá en la consola un mensaje
+        tan pronto como se cree un objeto House.
+        Defina una clase de prueba
+        (por ejemplo, Tester, Start u otro nombre razonable que prefiera) con un método principal donde:
+        declarar dos variables House house1 y house2
+        creas dos objetos House y los asignas a las variables house1 y house2
+         */
         //Creando mi objeto y asignando datos.
-        House h1 = new House("Giussepe Garibaldi, 4", 5);
-        //mostrando datos en pantalla
-        h1.mostrarDatos();
+        House house1, house2;
+
+        house1 = new House("Giussepe Garibaldi, 4", 5);
+        house2 = new House("Tibaldi, 15", 3);
     }
 }
